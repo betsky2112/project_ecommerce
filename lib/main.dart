880750e0af627d77d74ceeedec330d9da6b70bc7
betsky2112 <roberthussebastians@gmail.com>
2024-07-19 15:32:37 +0700

    utility_and_helper_functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_ecommerce/utils/theme/theme.dart';
+import 'package:project_ecommerce/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: EcommerceAppTheme.lightTheme,
-      darkTheme: EcommerceAppTheme.darkTheme,
-    );
-  }
 }
